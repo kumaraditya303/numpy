9a6fc3996400953f449b69ea89c43c73a0830a25
Kumar Aditya <kumaraditya@python.org>
2026-03-01 21:31:27 +0530

    wip

--- numpy/_core/src/multiarray/unique.cpp
+++ numpy/_core/src/multiarray/unique.cpp
@@ -63,7 +63,7 @@ empty_array_like(PyArrayObject *arr, npy_intp length)
 
 template <typename T>
 size_t hash_integer(const T *value, npy_bool equal_nan) {
-    return std::hash<T>{}(*value);
+    return npy_fnv1a(reinterpret_cast<const unsigned char*>(value), sizeof(T));
 }
 
 template <typename S, typename T, S (*real)(T), S (*imag)(T)>
@@ -266,7 +266,7 @@ unique_string(PyArrayObject *self, npy_bool equal_nan)
 
     PyArray_Descr *descr = PyArray_DESCR(self);
     // variables for the string
-    npy_intp itemsize = PyDataType_ELSIZE(descr);
+    npy_intp itemsize = descr->elsize;
     npy_intp num_chars = itemsize / sizeof(T);
 
     auto hash = [num_chars](const T *value) -> size_t {
